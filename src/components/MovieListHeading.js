eda5c95c6dad2fea756567098fb84bb1a34c9dcb
Uche Ogbonna <ucheogbonnak@gmail.com>
2022-12-03 20:14:50 +0100

    new

---
+++ src/components/MovieListHeading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MovieListHeading = (props) => {
+    return (
+        <div className="col">
+            <hi>{props.heading}</hi>
+        </div>
+    );
+};
+
+export default MovieListHeading;
